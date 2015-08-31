22b9550dcda68958eb85da00c1abdc561128c650
AHare <art.hare@am.sony.com>
2015-08-31 15:17:06 -0400

    simplifying.  You don't even need a component to leak.  Looks like simply transitioning in ember is sufficient.

--- app/components/leaky-component/component.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-
-  // creates an array from 0..500
-  wasteArray: Ember.computed(function() {
-    return Array.apply(null, {length: 500}).map(Function.call, Math.random);
-  }),
-});
